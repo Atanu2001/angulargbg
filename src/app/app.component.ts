da9f539d67f92c48eb2f6ee74ce4f12a1c7e3d57
atanu <atanu.rtech1@gmail.com>
2023-07-05 13:34:26 +0530

    login page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gbg';
+  usericon = faUser;
+  userpass = faLock;
 }
